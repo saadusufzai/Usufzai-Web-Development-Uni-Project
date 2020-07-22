85884be4a68b4476f2d0f9b1af457f5b62463b7f
Saad Aslam <saadusufzai@gmail.com>
2020-07-22 13:49:15 +0500

    ui done
    only php left

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.bars').addEventListener('click',()=>{
+    document.querySelector('.slider').classList.add('show')
+})
+
+document.querySelector('.close').addEventListener('click',()=>{
+    document.querySelector('.slider').classList.remove('show')
+})
+
+
+var menus = document.querySelector('.menues')
