a26ede88184272da6ac2645a425e88b7e46d583d
Saad Aslam <saadusufzai@gmail.com>
2020-07-23 12:52:46 +0500

    syle box 3 fixed

--- connect.php
+++ connect.php
@@ -1,9 +1,11 @@
 <?php
 
-	// $name = $_POST['name'];
-	// $email = $_POST['email'];
-	// $plan = $_POST['plan'];
-	// $method = $_POST['method'];
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$plan = $_POST['plan'];
+	$option = $_POST['option'];
+	$method = $_POST['method'];
 	
 
 	// Database connection
@@ -11,5 +13,17 @@
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
-	} 
-	?>
+	} else {
+		$stmt = $conn->prepare("insert into registration(name, email, package, pay_method) values( ?, ?, ?, ?)");
+		$stmt->bind_param($name, $email, $plan, $method);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
+
+
+
+	
