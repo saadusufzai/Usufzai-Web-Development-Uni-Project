71a0eea8640934dbf114d78bcd27b3a4aa615add
Saad Aslam <saadusufzai@gmail.com>
2020-08-09 17:03:01 +0500

    database connect

--- connect.php
+++ connect.php
@@ -1,29 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/connect.css">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+      integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
+      crossorigin="anonymous"
+    />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact us</title>
+</head>
+<body>
+    <div class="slider">
+        <div class="menus">
+          <i class="close fa fa-times fa-3x" aria-hidden="true"></i>
+          <ul>
+            <li><a href="./index.html">home  <span>home is where you start</span></a></li>
+            <li><a href="./about.html">about  <span>know more about our work</span> </a></li>
+            <li><a href="./order.html">order  <span>order your package now</span></a></li>
+            <li><a href="./contact.html">contact  <span>confused? contact us right now</span></a></li>
+          </ul>
+        </div>
+      </div>
+      <nav class="nav-bar">
+        <div class="inner-nav">
+            <div class="logo"><a href="./index.html">TECHWA</a></div>
+          <div class="menu">
+            <i class=" bars fa fa-bars fa-2x" aria-hidden="true"></i>
+          </div>
+        </div>
+	  </nav>
+	  
+	  
 <?php
+	
+$fname = $_POST['firstName'];
+$email = $_POST['email'];
+$package = $_POST['package'];
+$method = $_POST['method'];
+
+	$conn = new mysqli('localhost','root','','order');
+
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	  }
+		 $sql = "INSERT INTO plan(name,email,package,pay_method) VALUES ('$fname','$email','$package','$method')";
 
 
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$plan = $_POST['plan'];
-	$option = $_POST['option'];
-	$method = $_POST['method'];
+	if($conn->query($sql) === TRUE){
+		echo' <div class="success">
+				<div class="success-box">
+				<h1>Thanks for choosing TECHWA </h1>
+				<p>Your order has been placed</p>
+				<a href="./">Go to Home</a>
+				</div>
+		</div> 
+		';
+		}
+	else{
+		echo ' <h1>NOT SAVED</h1>';
+
+	}
 	
 
-	// Database connection
-	$conn = new mysqli('localhost','root','','order');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into registration(name, email, package, pay_method) values( ?, ?, ?, ?)");
-		$stmt->bind_param($name, $email, $plan, $method);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "Registration successfully...";
-		$stmt->close();
-		$conn->close();
-	}
 ?>
 
+	  <section class="see-more">
+        <p>Need help? Call our award-winning support team at +92 332 687 7676</p>
+      </section>
+      <footer>
+        <div class="inner-footer">
+            <ul>
+              <h3>Web Hosting</h3>
+              <li>CPanel</li>
+              <li>WHMS</li>
+              <li>DOMAIN</li>
+            </ul>
+            <ul>
+              <h3>Web Design Tools</h3>
+              <li>Adobe XD</li>
+              <li>Figma</li>
+              <li>PhotoShop</li>
+            </ul>
+            <ul>
+              <h3>VPS</h3>
+              <li>Cloud</li>
+              <li>Private Server</li>
+              <li>Dedicated Server</li>
+            </ul>
+            <ul>
+              <h3>Contact Us</h3>
+              <li> <i class="fa fa-map-marker" aria-hidden="true"></i> 120-N Mandi Town Bhakkar</li>
+              <li> <i class="fa fa-envelope" aria-hidden="true"></i>  saadusufzai@gmail.com</li>
+              <li> <i class="fa fa-link" aria-hidden="true"></i> www.techwa.io</li>
+              <li> <i class="fa fa-phone" aria-hidden="true"></i>  03326877676</li>
+            </ul>
+        </div>
+        <div class="social">
+          <input type="text" name="Newsletteer" id="newsletter" placeholder="Subscribe to Newsletters">
+        </div>
+  
+        <div class="copyright">
+          Copyright 2020  techwa.io | All rights are reserved by TECHWA.IO | Developed by Saad Aslam
+        </div>
+      </footer>
+      <script src="./main.js"></script>
+      <script src="./db_connection.js"></script>
+</body>
+</html>
 
-
-	
